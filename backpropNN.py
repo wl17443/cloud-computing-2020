99add374fc64f4b4409ed35e7ad6b3e338199b81
Orion <wl17443@bristol.ac.uk>
2020-12-08 10:47:02 +0000

    Updated backpropNN py script

--- backpropNN.py
+++ backpropNN.py
@@ -1,7 +1,10 @@
-import numpy as np 
 import os
-from mnistDataLoader import MnistDataloader
+import csv 
+import argparse
+import datetime 
+import numpy as np 
 import matplotlib.pyplot as plt 
+from mnistDataLoader import load_data
 
 # Define Neural Network class 
 class NeuralNetwork():
@@ -46,7 +49,6 @@ class NeuralNetwork():
     return weight_change
 
   def forward(self, inputs):
-    # print("Inputs: ", inputs)
     if len(self.Layers) == len(self.Structure):
       self.Layers[0] = inputs 
     else:
@@ -73,16 +75,15 @@ class NeuralNetwork():
 
   def update_weights(self):
     for i in range(len(self.Weights)):
-      # print("Weight change: ", self.ChangeInWeights[i])
       self.Weights[i] -= self.LearningRate*self.ChangeInWeights[i]
 
-  def calculate_accuracy(self, targets):
-    success = 0 
-    for i in range(len(self.Predictions)):
-      pred = np.argmax(self.Predictions[i])
-      if pred == np.argmax(targets[i]):
-        success += 1
-    return success/len(self.Predictions)
+  # def calculate_accuracy(self, targets):
+  #   success = 0 
+  #   for i in range(len(self.Predictions)):
+  #     pred = np.argmax(self.Predictions[i])
+  #     if pred == np.argmax(targets[i]):
+  #       success += 1
+  #   return success/len(self.Predictions)
 
   def train_one_batch(self, batch_number):
 
@@ -93,18 +94,29 @@ class NeuralNetwork():
     self.forward(inputs)
     self.backprop(inputs, targets)
     self.update_weights()
-    loss = 0.001*np.sum((self.Predictions-targets)**2)
-    accuracy = self.calculate_accuracy(targets)
-    return accuracy, loss
+    loss = (1/(self.BatchSize*10))*np.sum((self.Predictions-targets)**2)
+    return loss
   
   def train(self, nr_batches):
-    accuracies = []
     losses = []
     for i in range(nr_batches):
-      accuracy, loss = self.train_one_batch(i)
-      accuracies.append(accuracy)
+      loss = self.train_one_batch(i)
       losses.append(loss)
-    return accuracies, losses 
+    return losses 
+
+  def test(self):
+    successes = 0 
+    test_len = len(test_x)
+    for _ in range(int(test_len/self.BatchSize)):
+      inputs, targets = generate_batch(test_x, test_y, self.BatchSize) 
+      self.forward(inputs)
+      for j in range(self.BatchSize):
+        prediction = np.argmax(self.Predictions[j])
+        target = np.argmax(targets[j])
+        if prediction == target:
+          successes += 1
+    success_rate = successes/test_len
+    return success_rate
 
 def generate_batch(dataset_x, dataset_y, batch_size):
   rand_inds = np.random.randint(0, len(dataset_x), batch_size)
@@ -113,19 +125,28 @@ def generate_batch(dataset_x, dataset_y, batch_size):
   return inputs, outputs 
 
 if __name__ == "__main__":
-  cwd = os.getcwd()
-  input_path = os.path.join(cwd, 'MNIST-dataset/Datasets') 
-  training_images_filepath = os.path.join(input_path, 'train-images-idx3-ubyte')
-  training_labels_filepath = os.path.join(input_path, 'train-labels-idx1-ubyte')
-  test_images_filepath = os.path.join(input_path, 't10k-images-idx3-ubyte')
-  test_labels_filepath = os.path.join(input_path, 't10k-labels-idx1-ubyte')
 
-  mnist_dataloader = MnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath, test_labels_filepath)
+  parser = argparse.ArgumentParser(description='Training Parameters')
+  parser.add_argument('--batch_size', required =True, type=int, help='batch size')
+  parser.add_argument('--nr_batches', required =True, type=int, help='number of batches')
+  parser.add_argument('--learning_rate', required =True, type=float, help='learning rate')
+  parser.add_argument('--hidden_layers', required =True, nargs='*', type=int, help='hidden layer sizes')
 
-  (train_x, train_y), (test_x, test_y) = mnist_dataloader.load_data()
+  (train_x, train_y), (test_x, test_y) = load_data()
 
-  network = NeuralNetwork([784, 500, 100, 10], 10, 0.01)
-  accuracy, loss = network.train(5000)
+  args = parser.parse_args()
+  architecture = [784]
+  for i in range(len(args.hidden_layers)):
+    architecture.append(args.hidden_layers[i])
+  architecture.append(10)
   
-  plt.plot(range(5000), loss)
-  plt.show()
+  network = NeuralNetwork(architecture, int(args.batch_size), float(args.learning_rate))
+  loss = network.train(int(args.nr_batches))
+  success_rate = network.test()
+
+  # Write to EBS checkpoint data bank 
+  with open('/training/training-{}-{}-{}-{}.csv'.format(args.batch_size, args.nr_batches, args.learning_rate, *args.hidden_layers), mode='w') as data:
+    data_writer = csv.writer(data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    data_writer.writerow([success_rate])
+    data_writer.writerow(loss)
+    data_writer.writerows(network.Weights)
